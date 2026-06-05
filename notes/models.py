770f961dc6ab558262938542fda999c86d3f9262
Vyshnav K B <vyshnavkb648@gmail.com>
2026-06-05 14:19:04 +0530

    create Note Model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    is_pinned = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.title
+    
+
