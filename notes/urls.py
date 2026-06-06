68e48d6d693c04d1d32e477eba6cdcd3b330807f
Vyshnav K B <vyshnavkb648@gmail.com>
2026-06-06 23:50:18 +0530

    Add forms views and urls for notes app

---
+++ notes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('',views.note_list,name='note_list'),
+    path('create/',views.create_note,name='create_note'),
+    path('<int:pk>/',views.note_detail,name='note_detail'),
+    path('<int:pk>/pin/',views.toggle_pin,name='toggle_pin'),
+
+]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Note
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from .forms import NoteForm
 
-# Create your views here.
+def note_list(request):
+
+    notes = Note.objects.all().order_by(
+        '-is_pinned',
+        '-created_at'
+    )
+    return render(request,'notes/notes_list.html',{'notes': notes})
+
+def note_detail(request, pk):
+    
+    note = get_object_or_404(
+        Note,
+        pk=pk
+    )
+    return render(request,'notes/note_detail.html',{'note': note})
+
+def create_note(request):
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+        else:
+            form.Noteform()
+
+        return redirect(request,'notes/create_note.html',{'form':form})
+    
+def toggle_pin(request,pk):
+    if request.method == 'POST':
+        note = get_object_or_404(Note,pk=pk)
+        note.is_pinned = not note.is_pinned
+        note.save()
+
+    return redirect('note_list')
